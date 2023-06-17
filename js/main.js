67bbe108036540469b4a16e671f7a17eb4b53009
Shriya Challam <shriya.challam@duke.edu>
2023-06-17 11:32:00 -0700

    website v1

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
+
+let colors = ['rgb(193, 219, 151)','rgb(96, 116, 156)','rgb(201, 199, 199)'];
+let color = colors[Math.floor(Math.random() * 3)];
+document.getElementById("backg").style.backgroundColor = color;
+				
